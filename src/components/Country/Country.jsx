357587c8f3738a63b554aee6910b1bedfbc1deb7
rushomrong <rushomarak@gmail.com>
2024-10-21 05:13:38 +0600

    country population & area added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -4,13 +4,15 @@ const Country = ({ country }) => {
   // console.log(country);
   if (!country) return null;
 
-  const { name, flags } = country; //destructuring
+  const { name, flags, population, area } = country; //destructuring
 
   return (
     <div className="country">
       <h3>Name: {name?.common || "No Name Available"}</h3>
-      <h3>Short Code: {country?.cca3 || "Not Found the Short Code"} </h3>
       <img src={flags?.png} alt={`${name} flag`} />
+      <h3>Short Code: {country?.cca3 || "Not Found the Short Code"} </h3>
+      <h3>Population: {population}</h3>
+      <h3>Area: {area}</h3>
     </div>
   );
 };
